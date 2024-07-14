c77f0c22ca18aa92ea9bac8d159d88a23d03ca00
Insaf Inhaam <insafinhaam732@gmail.com>
2024-07-14 22:30:19 +0530

    working process update

--- front-page.php
+++ front-page.php
@@ -39,7 +39,11 @@ get_header();
                                     <a href="#">
                                         <!-- <img src="<?php //echo get_template_directory_uri() ?>/assets/img/icon/banner_icon01.svg"
                                             alt=""> -->
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="20" width="17.5" viewBox="0 0 448 512"><path fill="#acb5bc" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="20" width="17.5"
+                                            viewBox="0 0 448 512">
+                                            <path fill="#acb5bc"
+                                                d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+                                        </svg>
                                     </a>
                                 </li>
                                 <li>
@@ -291,7 +295,7 @@ get_header();
                     <div class="work-content">
                         <div class="section-title title-style-two white-title mb-30">
                             <span class="sub-title">Working Steps</span>
-                            <h2 class="title">Our Working Process</h2>
+                            <h2 class="title">Our Working Process for Video Editing </h2>
                         </div>
                         <div class="work-list">
                             <ul class="list-wrap">
@@ -302,8 +306,7 @@ get_header();
                                     </div>
                                     <div class="content">
                                         <h4 class="title">Concept Creation</h4>
-                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                        </p>
+                                        <p>We understand your vision and create a plan for your video.</p>
                                     </div>
                                 </li>
                                 <li>
@@ -312,8 +315,20 @@ get_header();
                                             alt="">
                                     </div>
                                     <div class="content">
-                                        <h4 class="title">Sketch Drawing</h4>
-                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                        <h4 class="title">Receiving and Verifying Footage </h4>
+                                        <p>We get all the video clips from you and check them to ensure they are ready
+                                            for editing.</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/icon/about_icon03.png"
+                                            alt="">
+                                    </div>
+                                    <div class="content">
+                                        <h4 class="title">Editing and Quality Check</h4>
+                                        <p>We start editing the video, piecing together the clips and adding effects. We
+                                            then review the edited video to ensure it meets high-quality standards.
                                         </p>
                                     </div>
                                 </li>
@@ -323,9 +338,8 @@ get_header();
                                             alt="">
                                     </div>
                                     <div class="content">
-                                        <h4 class="title">Final Design</h4>
-                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                        </p>
+                                        <h4 class="title">Final Design and Submission</h4>
+                                        <p>We deliver the polished, final video to you.</p>
                                     </div>
                                 </li>
                             </ul>
